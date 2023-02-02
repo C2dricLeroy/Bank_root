3a14fb2e977be7cef7afdf7559135a0e34ece9f4
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-02-02 14:24:03 +0100

    feat(bank_account): create bank_account folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module.js';
 import { CustomersModule } from './customers/customers.module';
+import { BankAccountModule } from './bank_account/bank_account.module';
 
 @Module({
-  imports: [AuthModule, CustomersModule],
+  imports: [AuthModule, CustomersModule, BankAccountModule],
   controllers: [AppController],
   providers: [AppService],
 })
