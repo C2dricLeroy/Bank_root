0dfeab4c678a77b8618b9019eba8a8ff10dd1fd0
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-02-03 13:44:41 +0100

    feat(common): set a global prefix

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,26 @@ import { AppModule } from './app.module';
 // import { Bank_accountService } from './bank_account/bank_account.service.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule, 
-  );
-  const config = new DocumentBuilder()    
-    .setTitle('Median')    
-    .setDescription('The Median API description')    
-    .setVersion('0.1')    
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  
+
   SwaggerModule.setup('api', app, document);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '../src/', 'views'));
   app.setViewEngine('hbs');
-  app.useGlobalPipes(new ValidationPipe({
-    forbidUnknownValues: false,
-    whitelist: true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidUnknownValues: false,
+      whitelist: true,
+    }),
+  );
 
   await app.listen(3333);
 }
