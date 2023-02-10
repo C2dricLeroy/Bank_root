3984c76bccce1e2000f732567366f85192dae9bc
Cdric2Leroy <cedric.leroy.psycho@gmail.com>
2023-02-10 16:35:27 +0100

    test(bank_account): test findAll

--- src/bank_account/bank_account.controller.spec.ts
+++ src/bank_account/bank_account.controller.spec.ts
@@ -1,20 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BankAccountController } from './bank_account.controller';
 import { BankAccountService } from './bank_account.service';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { PrismaClient } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
-describe.skip('BankAccountController', () => {
+describe('BankAccountController', () => {
   let controller: BankAccountController;
+  let prisma: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BankAccountController],
-      providers: [BankAccountService],
-    }).compile();
+      providers: [BankAccountService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
     controller = module.get<BankAccountController>(BankAccountController);
+    prisma = module.get(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of bank accounts', async () => {
+      const result = [
+        {
+          Id: 1,
+          Balance: 250,
+          RIB: 'testRIB1',
+          owner_id: 2,
+          admin_id: 2,
+          log_id: 0,
+        },
+      ];
+      prisma.bank_account.findMany.mockResolvedValue(result);
+      expect(controller).toBeDefined();
+    });
   });
 });
